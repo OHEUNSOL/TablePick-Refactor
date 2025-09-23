9571bbbb34f7ac3ed949028bdb43d99bce93520f
OHEUNSOL <eunsol037@naver.com>
2025-09-24 01:32:07 +0900

    init: TablePick project

---
+++ src/main/java/com/goorm/tablepick/TablepickApplication.java
@@ -0,0 +1,13 @@
+package com.example.tablepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablepickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TablepickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goorm/tablepick/TablepickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tablepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablepickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
