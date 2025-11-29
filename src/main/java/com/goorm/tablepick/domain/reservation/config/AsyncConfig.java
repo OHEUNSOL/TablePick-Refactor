31272d7785595db36659dd9b230213e30fc24745
OHEUNSOL <eunsol037@naver.com>
2025-11-30 03:55:56 +0900

    feat: 비동기용 스레드 설정

---
+++ src/main/java/com/goorm/tablepick/domain/reservation/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.goorm.tablepick.domain.reservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "mailTaskExecutor")
+    public Executor mailTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);      // 기본 실행 대기 스레드 수
+        executor.setMaxPoolSize(10);      // 최대 스레드 수
+        executor.setQueueCapacity(100);    // 최대 큐 크기
+        executor.setThreadNamePrefix("mail-async-");
+        executor.initialize();
+        return executor;
+    }
+}
