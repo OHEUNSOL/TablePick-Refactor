1ee69c90e028b8af540d420da5ad9e6abca6c474
OHEUNSOL <eunsol037@naver.com>
2025-12-11 17:05:28 +0900

    refactor: 변수 추가

--- src/main/java/com/goorm/tablepick/domain/restaurant/entity/RestaurantImage.java
+++ src/main/java/com/goorm/tablepick/domain/restaurant/entity/RestaurantImage.java
@@ -17,13 +17,17 @@ public class RestaurantImage {
     @Column(nullable = false)
     private String imageUrl;
 
+    @Column(nullable = false)
+    private Integer sortOrder;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
 
     @Builder
-    public RestaurantImage(String imageUrl, Restaurant restaurant) {
+    public RestaurantImage(String imageUrl, Restaurant restaurant,  Integer sortOrder) {
         this.imageUrl = imageUrl;
         this.restaurant = restaurant;
+        this.sortOrder = sortOrder;
     }
 }
