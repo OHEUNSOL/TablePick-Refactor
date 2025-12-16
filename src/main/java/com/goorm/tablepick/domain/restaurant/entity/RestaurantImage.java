d9fff39e5b8b170766fcfaf367fb7900317536f5
OHEUNSOL <eunsol037@naver.com>
2025-12-16 12:04:16 +0900

    refactor: 컬럼 삭제

--- src/main/java/com/goorm/tablepick/domain/restaurant/entity/RestaurantImage.java
+++ src/main/java/com/goorm/tablepick/domain/restaurant/entity/RestaurantImage.java
@@ -17,17 +17,13 @@ public class RestaurantImage {
     @Column(nullable = false)
     private String imageUrl;
 
-    @Column(nullable = false)
-    private Integer sortOrder;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
 
     @Builder
-    public RestaurantImage(String imageUrl, Restaurant restaurant,  Integer sortOrder) {
+    public RestaurantImage(String imageUrl, Restaurant restaurant) {
         this.imageUrl = imageUrl;
         this.restaurant = restaurant;
-        this.sortOrder = sortOrder;
     }
 }
